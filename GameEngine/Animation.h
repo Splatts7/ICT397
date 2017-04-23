732c111d5fb419fc22d5297081d1a111a50d6e7f
Jonathan Sands <cosmictails@gmail.com>
2017-04-23 11:00:43 +0800

    AI and Animation class templates added
    Plus a small fix for the Input class for rendering the wireframe, and
    the mouse cursor is now hidden.

---
+++ GameEngine/Animation.h
@@ -0,0 +1,32 @@
+/**
+* @class Animation
+* @brief Animation engine to handle all GameObject animations
+* @author Jonathan Sands
+* @version 01 - Started
+* @date 23/04/2017
+*/
+
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+class Animation
+{
+	public:
+
+		/**
+		* @brief Defualt constructor
+		*/
+		Animation();
+
+		/**
+		* @brief Deconstructor
+		*/
+		~Animation();
+
+	private:
+
+
+
+};
+
+#endif
