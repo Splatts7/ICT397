2ec1b82d82000bd2d03e744ec9df3fa83d026810
Splatts7 <jack_splatters@hotmail.com>
2017-04-28 14:22:51 +0800

    Physics

---
+++ Debug/Scripts/CollisionShapes.lua
@@ -0,0 +1,40 @@
+-- CollisionShapes.lua
+-- Script for loading in data for collision shapes
+-- Author - Jack Matters
+-- Date - 27/04/2017
+shapes=
+{
+  sphere=
+  {
+    radius = 3
+  },
+  box=
+  {
+    x = 25.0,
+    y = 25.0,
+    z = 25.0,
+  },
+  cone=
+  {
+    radius = 3.0,
+    height = 5.0,
+  },
+  cylinder=
+  {  
+    radius = 3.0,
+    height = 5.0,
+  },
+  capsule=
+  {
+    radius = 3.0,
+    height = 5.0,
+  },
+  convex=
+  {
+    
+  },
+  heightfield=
+  {
+
+  }
+}
